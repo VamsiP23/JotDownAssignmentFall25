810847eeacd9f1a5fb3acc640280c18fd79c039a
Sri Vamsi Putti <90874211+VamsiP23@users.noreply.github.com>
2025-09-17 12:24:40 -0400

    Create Assignment

---
+++ JotDownAssignmentFall25/JotDownAssignmentFall25/JotDownAssignmentFall25App.swift
@@ -0,0 +1,17 @@
+//
+//  JotDownAssignmentFall25App.swift
+//  JotDownAssignmentFall25
+//
+//  Created by Vamsi Putti on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct JotDownAssignmentFall25App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
